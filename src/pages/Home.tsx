df026634617bea4de0d2d05d7baaa6dfb0e0250a
Yuto Tachibana <tachibanayu24@gmail.com>
2023-08-21 21:58:51 +0900

    Setup tailwind

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,10 +2,12 @@ import { Link } from 'react-router-dom'
 
 export const Home = () => {
   return (
-    <div>
+    <div className="bg-slate-400 text-xs">
       this is home page <br />
       <Link to="/result">go to result page</Link> <br />
-      <a href="/result">Anchor tag</a>
+      <a href="/result" className="text-blue-600">
+        Anchor tag
+      </a>
     </div>
   )
 }
